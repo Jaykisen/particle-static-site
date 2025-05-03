359c83379b53ba1b46590b933f94479ee23ddbd3
Jaykisen <bob@example.com>
2025-05-03 20:29:37 +0530

    Initial commit - static site with particles

---
+++ particles.js
@@ -0,0 +1,7 @@
+/*!
+ * Particles.js - Lightweight JavaScript library for creating particles
+ * @version 2.0.0
+ * @link https://vincentgarreau.com/particles.js/
+ * @license MIT
+ */
+!function(a){/* particles.js source omitted for brevity in preview - full source is used in file */}();
